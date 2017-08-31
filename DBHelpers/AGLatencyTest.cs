6d355182697caacb3c7044ab836ce89559c11513
Akhil Deshpande <deshpandeakhil@gmail.com>
2017-08-31 11:40:22 -0400

    Initial Checkin

---
+++ DBHelpers/AGLatencyTest.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DBHelpers
+{
+    public class AGLatencyTest
+    {
+        public IDBInfo TestLatency(IDBInfo item)
+        {
+            var result = IsReplicaInSync(item.PrimaryConnStr, item.ReplicaConnStr, item.Query).Result;
+            item.InSync = result.InSync;
+            item.PrimaryVal = result.PrimaryVal;
+            item.ReplicaVal = result.ReplicaVal;
+
+            return item;
+        }
+
+        private async Task<(bool InSync, string PrimaryVal, string ReplicaVal)> IsReplicaInSync(string primaryConnStr, string replicaConnStr, string query)
+        {
+            // make async call at the same time to both dbs
+            var primaryTask = GetFromDBAsync(primaryConnStr, query);
+            var replicaTask = GetFromDBAsync(replicaConnStr, query);
+
+            // wait here to get both values
+            string[] values = await Task.WhenAll(primaryTask, replicaTask);
+
+            // compare dates
+            if (string.IsNullOrWhiteSpace(values[0]) || string.IsNullOrWhiteSpace(values[1]))
+                throw new Exception("Error getting one of primary or replica value.");
+            else
+                return (values[0] == values[1], values[0], values[1]);
+        }
+
+        private async Task<string> GetFromDBAsync(string connStr, string query)
+        {
+            Object obj = null;
+
+            using (var conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+                var cmd = new SqlCommand(query, conn) { CommandType = CommandType.Text };
+                obj = await cmd.ExecuteScalarAsync();
+            }
+
+            if (obj != null)
+            {
+                return obj.ToString();
+            }
+            return null;
+        }
+    }
+
+    public class DBInfo : IDBInfo
+    {
+        public string PrimaryKey { get; set; }
+        public string PrimaryConnStr { get; set; }
+        public string ReplicaKey { get; set; }
+        public string ReplicaConnStr { get; set; }
+        public string Query { get; set; }
+        public bool InSync { get; set; }
+        public string PrimaryVal { get; set; }
+        public string ReplicaVal { get; set; }
+    }
+
+    public interface IDBInfo
+    {
+        string PrimaryKey { get; set; }
+        string PrimaryConnStr { get; set; }
+        string ReplicaKey { get; set; }
+        string ReplicaConnStr { get; set; }
+        string Query { get; set; }
+        bool InSync { get; set; }
+        string PrimaryVal { get; set; }
+        string ReplicaVal { get; set; }
+    }
+}
---
+++ DBHelpers/ConnectionTest.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Threading;
+using System.Data.SqlClient;
+using System.IO;
+using System.Net;
+using System.Runtime.CompilerServices;
+using System.Timers;
+
+namespace DBHelpers
+{
+    public class ConnectionInfo : IConnectionInfo
+    {
+        public string CommandText { get; set; }
+        public string ConnectionString { get; set; }
+        public long MSToOpenConnection { get; set; }
+        public long MSToExecute { get; set; }
+        public long MSTotalTime { get; set; }
+        public string Result { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+
+    public interface IConnectionInfo
+    {
+        string CommandText { get; set; }
+        string ConnectionString { get; set; }
+        long MSToOpenConnection { get; set; }
+        long MSToExecute { get; set; }
+        long MSTotalTime { get; set; }
+        string Result { get; set; }
+        string ErrorMessage { get; set; }
+    }
+
+    public class ConnectionTest
+    {
+        public IConnectionInfo ExecuteScalar(IConnectionInfo info)
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            try
+            {
+                var swConn = new Stopwatch();
+                swConn.Start();
+                using (var conn = new SqlConnection(info.ConnectionString))
+                {
+                    conn.Open();
+                    swConn.Stop();
+                    info.MSToOpenConnection = swConn.ElapsedMilliseconds;
+
+                    var cmd = new SqlCommand(info.CommandText, conn) { CommandType = CommandType.Text };
+
+                    var swExec = new Stopwatch();
+                    swExec.Start();
+                    info.Result = cmd.ExecuteScalar().ToString();
+                    swExec.Stop();
+                    info.MSToExecute = swExec.ElapsedMilliseconds;
+                }
+            }
+            catch (Exception ex)
+            {
+                info.ErrorMessage = ex.Message;
+            }
+            sw.Stop();
+            info.MSTotalTime = sw.ElapsedMilliseconds;
+
+            return info;
+        }
+    }
+}
